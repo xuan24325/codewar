cb489a73c45c6b23d702f2d28cc984256dc1c7f7
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-08 23:07:52 -0700

    Update code.py

--- code.py
+++ code.py
@@ -52,3 +52,21 @@ def duplicate_count(s):
       
 # len(string) -len(set(string))    
 
+#5
+aden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+      
+def to_jaden_case(string):
+	split_text=string.split()
+	revised_text=[]
+	for i in split_text:
+		revised_text.append(i.capitalize()[0]+i[1:])
+	returned_text=" ".join(revised_text)
+	return returned_text
+
