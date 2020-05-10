cf8a6880f0676700fa552945ad93ecd086369056
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-09 22:39:50 -0700

    Update code.py

--- code.py
+++ code.py
@@ -77,4 +77,23 @@ import string
 
 def toJadenCase(aaa):
     return string.capwords(aaa)
+#6
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+def spin_words(sentence):
+    sentence = sentence.split(' ')
+    for i in range(len(sentence)):
+        if len(sentence[i]) > 4:
+            sentence[i] = ''.join(reversed(sentence[i]))
+    return ' '.join(sentence)
+
+print(spin_words("Welcome"))    # => "emocleW"
+
+
+
+def spin_words(sentence):
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+
+	
