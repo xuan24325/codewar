12d99bfcd760a2529d861c4905fa073d813d527b
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-21 15:13:05 -0700

    Update code.py

--- code.py
+++ code.py
@@ -125,3 +125,9 @@ def unique_in_order(iterable):
             prev = char
     return result
 
+# groupby
+from itertools import groupby
+
+def unique_in_order(iterable):
+    return [k for (k, _) in groupby(iterable)]
+
