f21a746d04982d07493199326be1725293524368
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-22 14:22:15 -0700

    Update code.py

--- code.py
+++ code.py
@@ -131,3 +131,35 @@ from itertools import groupby
 def unique_in_order(iterable):
     return [k for (k, _) in groupby(iterable)]
 
+#7 kadane's algorithm
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+def max_sequence(arr):
+
+    sums = []
+    for j, e in enumerate(arr):
+        sums.append(e) 
+        for i in range(j + 1, len(arr)): 
+            e += arr[i]
+            sums.append(e)
+    if all(i<0 for i in arr):
+        return 0
+    else:
+        max_sum = max(sums)
+        return max_sum
+
+def max_sequence(arr):
+
+    max,curr=0,0
+    for x in arr:
+        curr+=x
+        if curr<0:curr=0
+        if curr>max:max=curr
+    return max
+
