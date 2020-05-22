949c55660a6427527d32c2e307b462ef6988fc6f
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-22 14:38:12 -0700

    Update code.py

--- code.py
+++ code.py
@@ -163,3 +163,28 @@ def max_sequence(arr):
         if curr>max:max=curr
     return max
 
+def maxSequence(arr):
+    maxl = 0
+    maxg = 0
+    for n in arr:
+        maxl = max(0, maxl + n)
+        maxg = max(maxg, maxl)
+    return maxg
+
+def maxSequence(arr):
+    current = 0
+    max_found = 0
+    
+    for value in arr:
+        current += value
+        if current < 0:
+            current = 0
+        
+        if current > max_found:
+            max_found = current
+    
+    return max_found
+
+def max_sequence(arr): 
+
+    return max([sum(arr[i:j]) for i in range(len(arr)+1) for j in range(len(arr)+1)])
