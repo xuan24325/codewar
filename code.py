e25e3e32adf3b24ca322624cd8dade3229b8ed65
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-23 18:34:14 -0700

    Update code.py

--- code.py
+++ code.py
@@ -177,3 +177,22 @@ def max_subarray(numbers):
 def max_sequence(arr): 
 
     return max([sum(arr[i:j]) for i in range(len(arr)+1) for j in range(len(arr)+1)])
+
+#8
+Task
+Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
+Example
+  delete_nth ([1,1,1,1],2) # return [1,1]
+
+  delete_nth ([20,37,20,21],1) # return [20,37,21]
+
+def delete_nth(order,max_e):
+    resp = []
+    for i in order:
+        if resp.count(i) < max_e:
+            resp.append(i)
+    return resp
+
+def delete_nth(order,max_e):
+    return [o for i,o in enumerate(order) if order[:i].count(o)<max_e ] 
