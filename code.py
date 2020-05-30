9d7712fc13ccb18b4114bddf9e203d1bd1e15212
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-30 13:31:18 -0700

    Update code.py

--- code.py
+++ code.py
@@ -227,3 +227,26 @@ def likes(names):
 	    3:  '{}, {} and {} like this',
 	    4:  '{}, {} and {others} others like this'
 	}[min(4, n)].format(*names[:3], others=n-2)
+
+#10
+Once upon a time, on a way through the old wild mountainous west,…
+… a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+
+Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadfull weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+
+How I crossed a mountain desert the smart way.
+The directions given to the man are, for example, the following (depending on the language):
+
+def dirReduc(arr):
+    opposites = [{'NORTH', 'SOUTH'}, {'EAST', 'WEST'}]
+    
+    for i in range(len(arr)-1):
+        if set(arr[i:i+2]) in opposites:
+            del arr[i:i+2]
+            return dirReduc(arr)
+    
+    return arr 
+
+arr=["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+print(dirReduc(arr))
+
