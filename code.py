b2746b97807407ca784dfd416c2996e507b258a2
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-09-10 17:30:03 -0700

    Update code.py

--- code.py
+++ code.py
@@ -282,3 +282,27 @@ def wave(t):
 t="hello world"    
 print(wave(t))
 
+#12
+Extract the domain name from a URL
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+domain_name("http://github.com/carbonfive/raygun") == "github" 
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+
+def domain_name(url):
+    return url.split("//")[-1].split("www.")[-1].split(".")[0]
+
+import re
+def domain_name(url):
+    return re.search('(https?://)?(www\d?\.)?(?P<name>[\w-]+)\.', url).group('name')
+
+def domain_name(url):
+    url = url.replace('www.','')
+    url = url.replace('https://','')
+    url = url.replace('http://','')
+    
+    return url[0:url.find('.')]
+
+def domain_name(url):
+    return url.split("://")[-1].split(".")[-2]
+
